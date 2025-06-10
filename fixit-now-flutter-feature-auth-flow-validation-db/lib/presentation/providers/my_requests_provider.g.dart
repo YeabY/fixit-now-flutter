e5322fdadf1ed2734071ac41450237d28878ba99
Abenezer  Elias <140536171+Abenezerleo@users.noreply.github.com>
2025-06-10 21:57:52 +0300

    Add files via upload

---
+++ fixit-now-flutter-feature-auth-flow-validation-db/lib/presentation/providers/my_requests_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'my_requests_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$myRequestsNotifierHash() =>
+    r'3e75a707578160a8a847ba9af16e5a1148ad8547';
+
+/// See also [MyRequestsNotifier].
+@ProviderFor(MyRequestsNotifier)
+final myRequestsNotifierProvider = AutoDisposeAsyncNotifierProvider<
+    MyRequestsNotifier, List<Request>>.internal(
+  MyRequestsNotifier.new,
+  name: r'myRequestsNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$myRequestsNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$MyRequestsNotifier = AutoDisposeAsyncNotifier<List<Request>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
